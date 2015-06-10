6cbf15e9ce5b5eaa0bdfc490bebb1ed09349802f
nliapis <nliapis1983@gmail.com>
2015-06-11 00:38:52 +0300

    connect with mongoldb

--- common/models/donor.js
+++ common/models/donor.js
@@ -1,3 +1,7 @@
 module.exports = function(Donor) {
-
+    Donor.beforeRemote('create', function(context, donor, next) {
+        console.log('Saving new donor with name: ', context.req.body.name);
+    
+        next();
+    });
 };
--- common/models/gift.js
+++ common/models/gift.js
@@ -1,3 +1,58 @@
 module.exports = function(Gift) {
 
+    // method which lists all free gifts
+    Gift.listFree = function(cb) {
+        Gift.find({
+            fields: {
+                reserved: false
+            }
+        }, cb);
+    };
+
+    // expose the above method through the REST
+    Gift.remoteMethod('listFree', {
+        returns: {
+            arg: 'gifts',
+            type: 'array'
+        },
+        http: {
+            path: '/list-free',
+            verb: 'get'
+        }
+    });
+
+    // method to return if the gift is free
+    Gift.isFree = function(id, cb) {
+        var response;
+        Gift.find({
+            fields: {
+                id: id
+            }
+        }, function(err, gift) {
+            if (err) return cb(err);
+
+            if (gift.reserved)
+                response = 'Sorry, the gift is reserved';
+            else
+                response = 'Great, this gift can be yours';
+
+        });
+        cb(null, response);
+    };
+
+    // expose the method through REST
+    Gift.remoteMethod('isFree', {
+        accepts: {
+            arg: 'id',
+            type: 'number'
+        },
+        returns: {
+            arg: 'response',
+            type: 'string'
+        },
+        http: {
+            path: '/free',
+            verb: 'post'
+        }
+    });
 };
